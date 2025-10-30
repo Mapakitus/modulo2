4d7a36c8d845d72a1bb7faa886e1ddd2fc51d241
Mapakitus <mapakitus@gmail.com>
2025-10-30 12:05:35 +0100

    ejemplos de validacion metodo POST

--- pydantic/validacion_basica.py
+++ pydantic/validacion_basica.py
@@ -11,6 +11,8 @@ class Usuario(BaseModel):
     nombre: str
     email: str
     edad: int
+    activo: bool
+
 
 #modelo con campos opcionales y valores por defecto
 class Producto(BaseModel):
@@ -19,6 +21,20 @@ class Producto(BaseModel):
     descripcion: Optional[str] = None
     disponible: bool = True
 
+@app.post("/usuarios")
+def crear_usuario(usuario: Usuario):
+    return {
+        "mensaje": f"Usuario {usuario.nombre} creado correctamente"      
+    }
+
+@app.post("/productos")
+def crear_producto(producto: Producto):
+    return {
+        "producto_creado": producto.nombre,
+        "precio": producto.precio,
+        "descripcion": producto.descripcion,
+        "disponible": producto.disponible
+    }
 
 
     
