f64eb0a2af9c4ece70ba6a5f38e5bdc7ad7b3c92
Mapakitus <mapakitus@gmail.com>
2025-11-03 11:30:14 +0100

    metodo POST

---
+++ metodos_POST/post.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Usuario(BaseModel):
+    nombre: str
+    email: str
+    edad: int
+    
+    
+@app.post("/usuarios/")
+async def crear_usuario(usuario: Usuario):
+    return{
+        "mensaje": f"Usuario {usuario.nombre} creado correctamente",
+        "datos": {
+            "nombre": usuario.nombre,
+            "email": usuario.email,
+            "edad": usuario.edad
+        }
+    }
+    
+class Contacto(BaseModel):
+    nombre: str
+    telefono: str
+    email: str
+    
+@app.post("/contactos/")
+async def crear_contacto(contacto: Contacto):
+    
+        respuesta = f"Gracias {contacto.nombre} hemos enviado la confirmación a {contacto.email}"
+        
+        return {"respuesta": respuesta}
+        
+        
+        
+      
