65c9b92a27466ae927d615cecc1f451d0ed2b416
Mapakitus <mapakitus@gmail.com>
2025-11-11 10:22:15 +0100

    modelo de base de datos SQLAlchemy

--- sqlalchemy/song.py
+++ sqlalchemy/song.py
@@ -22,3 +22,23 @@ SessionLocal = sessionmaker(
     bind=engine
 )
 
+#modelo base de datos (sqlalchemy)
+
+#clase base para modelos sqlalchemy, esto es para metadatos de las tablas
+class Base(DeclarativeBase):
+    pass
+
+#modelo de la tabla song
+class Song(Base):
+    __tablename__ = "songs"
+    
+    #Clave primaria
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    #requerido máximo 200 caracteres
+    title: Mapped[str] = mapped_column(String(200), nullable=False)
+    #requerido máximo 200 caracteres
+    artist: Mapped[str] = mapped_column(String(200), nullable=False)
+    #opcional
+    duration_seconds: Mapped[int | None] = mapped_column(Integer, nullable=True)
+    #opcional
+    explicit: Mapped[bool | None] = mapped_column(Boolean, nullable=True)
